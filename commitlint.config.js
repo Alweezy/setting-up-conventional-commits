ad0029851af80582a997759406a6c6f0881231de
Alweezy <alvinmutisya@gmail.com>
2025-10-21 19:50:13 +0300

    feat(config): reinforce conventional commits
    - Add commitlint configuration

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'api',
+        'ui',
+        'auth',
+        'database',
+        'config',
+        'ci',
+      ],
+    ],
+    'header-max-length': [2, 'always', 50],
+    'header-min-length': [2, 'always', 5]
+  },
+};
