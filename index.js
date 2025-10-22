ebae4fbc17047357d1c9d6e476e95e7c384bee0e
Alweezy <alvinmutisya@gmail.com>
2025-10-22 11:52:24 +0300

    feat(core): add a simple index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello from index.js!");
+
+const greetings = "Welcome to some simple application.";
+console.log(greetings);
+
+module.exports.sayGoodbye = () => {
+  console.log("Goodbye!");
+};
