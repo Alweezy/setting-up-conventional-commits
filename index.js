0ab183b43bee1889aa0b8001df63f45cb5ef9ecd
Alweezy <alvinmutisya@gmail.com>
2025-10-22 12:29:51 +0300

    feat(api): introduce a greetings api
    -Add a testing greetings api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-console.log("Hello from index.js!");
 
-const greetings = "Welcome to some simple application.";
-console.log(greetings);
+const express = require('express');
+const app = express();
+const port = 3000;
 
-module.exports.sayGoodbye = () => {
-  console.log("Goodbye!");
-};
+app.get('/api/v1/say-goodbye', (req, res) => {
+  res.json({ message: "Goodbye! This is the old V1 function name." });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
