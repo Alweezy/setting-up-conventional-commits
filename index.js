522af8d0b7d0db65ecb52378c8438d36fcb51c4f
Alweezy <alvinmutisya@gmail.com>
2025-10-22 12:31:58 +0300

    feat(api): rename greetings api
    - Update api naming conventions
    
    BREAKING CHANGE: Rename say-goodbye endpoint to goodbye

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/say-goodbye', (req, res) => {
+app.get('/api/v1/goodbye', (req, res) => {
   res.json({ message: "Goodbye! This is the old V1 function name." });
 });
 
